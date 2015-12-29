f128542beaa5c8f93971a5af668db3b8263c3a5a
Matteo <matteo.maffetti@gmail.com>
2015-12-29 12:32:32 +0100

    method eval and init

--- lab3/foglio3_3.py
+++ lab3/foglio3_3.py
@@ -1,1 +1,39 @@
 import functools
+import operator
+
+class PolishCalculator:
+
+    def __init__(self):
+        self._stack = []
+        self._operand = { '+': operator.add, '-': operator.sub, '*': operator.mul, '**': operator.pow, 'or': operator.or_, 'not': operator.not_, 'and': operator.and_ }
+        self.result = 0
+
+    def eval(self, str):
+        l = list(str)
+        l.reverse()
+
+        for el in l:
+            if (el.isdigit() == True) or ((el in self._operand.keys()) == True):
+                if (el.isdigit() == True):
+                    new = float(el)
+                else:
+                    new = el
+                self._stack.append(new)
+
+        while len(self._stack) > 0:
+            buff = []
+            x = self._stack.pop()
+
+            while (x in self._operand.keys()) == False:
+                buff.append(x)
+                x = self._stack.pop()
+
+            tot = functools.reduce(self._operand[x], buff)
+            self.result = self._operand[x](self.result, tot)
+
+        return self.result
+
+    def __str__(self):
+        s = ""
+        
+        return s
