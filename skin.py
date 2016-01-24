3c15576a85c7e3b5e4d2e6ebdbc97c979d4957b3
Matteo <matteo.maffetti@gmail.com>
2016-01-24 20:08:23 +0100

    update of types.MethodType

--- skin.py
+++ skin.py
@@ -1,7 +1,8 @@
+import types
 
 def become(obj, s_m_add, s_m_del):
     for m in s_m_add:
-        setattr(obj, m.__name__, m)
+        setattr(obj, m.__name__, types.MethodType(m, obj))
     for m in s_m_del:
         delattr(obj, m.__name__)
 
@@ -9,6 +10,16 @@ class Skin(type):
     def __new__(meta, clsname, supers, clsdict):
         clsdict['become'] = become
         return type.__new__(meta, clsname, supers, clsdict)
+
+def pop(self):
+  top = self.data[-1]
+  self.data = self.data[:-1]
+  self.top -= 1
+  return top
+
+def push(self, v):
+  self.data += [v]
+  self.top += 1
 
 class stack(metaclass=Skin):
     def __init__(self, dim=10):
